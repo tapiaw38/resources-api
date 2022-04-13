1e56ea3aa1b1a266120e61fa183d7af240d74911
walter <walter.tapia@bigbox.com.ar>
2022-04-13 01:06:36 -0300

    fix: repair delete

--- database/employee/delete_employee.go
+++ database/employee/delete_employee.go
@@ -13,48 +13,21 @@ func DeleteEmployee(ctx context.Context, id string) (models.Employee, error) {
 	var employee models.Employee
 
 	q := `
-	DELETE FROM employee
+		DELETE FROM employee
 		WHERE id = $1
-		RETURNING id, file_code, agent_number, first_name, last_name, document_number,
-			birth_date, date_admission, phone, address, picture, salary, category, status, work_number,
-			employee_type, workplace, created_at, updated_at;
+		RETURNING id
 	`
 
 	rows := database.Data().QueryRowContext(
 		ctx, q, id,
 	)
 
-	var fileCode, documentNumber models.NullString
-	var birthDate, dateAdmission models.NullString
-	var workplace models.NullInt64
+	err := rows.Scan(&employee.ID)
 
-	err := rows.Scan(
-		&employee.ID,
-		&fileCode,
-		&employee.AgentNumber,
-		&employee.FirstName,
-		&employee.LastName,
-		&documentNumber,
-		&birthDate,
-		&dateAdmission,
-		&employee.Phone,
-		&employee.Address,
-		&employee.Picture,
-		&employee.Salary,
-		&employee.Category,
-		&employee.Status,
-		&employee.WorkNumber,
-		&employee.EmployeeType,
-		&workplace,
-		&employee.CreatedAt,
-		&employee.UpdatedAt,
-	)
-
-	employee.FileCode = fileCode.String
-	employee.DocumentNumber = documentNumber.String
-	employee.BirthDate = birthDate.String
-	employee.DateAdmission = dateAdmission.String
-	employee.Workplace = workplace.Int64
+	if err != nil {
+		return employee, err
+	}
 
 	return employee, err
+
 }
